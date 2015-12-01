20b364c7dfd16db21cf97b3ae1609027e89685af
micheal yang <linstudyhard@163.com>
2015-12-01 19:23:32 +0800

    Create url.js

---
+++ url.js
@@ -0,0 +1,4 @@
+//获取URL中的参数值
+function getURLParameter(name) {
+		return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search)||[,""])[1].replace(/\+/g, '%20'))||null;
+	}
